9cd40e23079358f32b1e82aca108e6ab3d93d1ba
Sergey Berezovsky <sb206@students.uwf.com>
2021-03-24 23:52:15 -0500

    Finished Implementing the graphical part of the project

--- GUI.py
+++ GUI.py
@@ -16,14 +16,52 @@ class Window:
 
     def drawCircles(self):
         for circle in range(0, self.numStates):
-            self.drawCircle(120, 50+(circle*120), 180, 50+(circle*120)+60)
+            self.drawCircle(120, 20+(circle*120), 180, 20+(circle*120)+60)
             if(circle in self.acceptStates):
-                self.drawCircle(125, 55+(circle*120), 175, 55+(circle*120)+50)
-            self.canvas.create_text(150, 50+(circle*120)+30, text=str(circle))
+                self.drawCircle(125, 25+(circle*120), 175, 25+(circle*120)+50)
+            self.canvas.create_text(150, 20+(circle*120)+30, text=str(circle))
+
+
+    def drawTransitions(self):
+        transitions = self.fsa.getTransitions()
+        for which in range(0, len(transitions)):
+            transition = transitions[which].split(':')
+
+            if(int(transition[1])==(int(transition[0])+1)):
+                self.canvas.create_line(150, 20+60+int(transition[0])*120, 150, 20+60+int(transition[0])*120+60, arrow = tk.LAST)
+                self.canvas.create_text(160, 50+60+int(transition[0])*120, text = transition[2])
+
+            elif(int(transition[1])==(int(transition[0])-1)):
+                self.canvas.create_line(150, 20+60+int(transition[1])*120, 150, 20+60+int(transition[1])*120+60, arrow = tk.FIRST)
+                self.canvas.create_text(160, 50+60+int(transition[1])*120, text = transition[2])
+
+            elif(int(transition[1])<(int(transition[0])-1)):
+                print(transition[0],":", transition[1])
+                self.canvas.create_line(100, 20+30+int(transition[1])*120, 100, 20+30+int(transition[0])*120)
+                self.canvas.create_line(100, 20+30+int(transition[1])*120, 120, 20+30+int(transition[1])*120, arrow=tk.LAST)
+                self.canvas.create_line(100, 20+30+int(transition[0])*120, 120, 20+30+int(transition[0])*120)
+                self.canvas.create_text(90, 50+60+int(transition[1])*120, text = transition[2])
+
+            elif(int(transition[1])>(int(transition[0])+1)):
+                self.canvas.create_line(100, 20+30+int(transition[1])*120, 100, 20+30+int(transition[0])*120)
+                self.canvas.create_line(100, 20+30+int(transition[1])*120, 120, 20+30+int(transition[1])*120, arrow = tk.LAST)
+                self.canvas.create_line(100, 20+30+int(transition[0])*120, 120, 20+30+int(transition[0])*120)
+                self.canvas.create_text(90, 50+60+int(transition[0])*120, text = transition[2])
+
+            elif(int(transition[1])==(int(transition[0]))):
+                self.drawCircle(160, 30+int(transition[1])*120, 200, 30+int(transition[1])*120+40)
+                self.canvas.create_line(170, 30+int(transition[1])*120, 171, 30+int(transition[1])*120, arrow = tk.FIRST)
+                self.canvas.create_text(210, 20+30+int(transition[0])*120, text = transition[2])
+           
+
+       
 
 
     def showFSA(self):
         self.root.wm_title("FSA")
+        self.canvas.create_line(115, 15, 130, 30, arrow = tk.LAST)
+        self.drawTransitions()
         self.drawCircles()
+       
         self.canvas.pack()
         self.root.mainloop()
